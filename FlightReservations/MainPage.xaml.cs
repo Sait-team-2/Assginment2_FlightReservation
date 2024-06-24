8e9c4ffec752c46b4ccec75d8993a1026ad0f232
marian.estrada <ian_an@yahoo.com>
2024-06-24 16:44:11 -0600

    Upload Base Code

---
+++ FlightReservations/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightReservations
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
