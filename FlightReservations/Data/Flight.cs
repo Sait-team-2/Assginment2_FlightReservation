36f73a0036e665207d3977cb1a63871af0580a5d
AReddy <155758262+K0A1R@users.noreply.github.com>
2024-06-30 17:20:38 -0600

    First Commit
    Add flights.csv, airports.csv to project
    Add Data folder
    Add Flight.cs, FlightManager.cs to Data folder
    Add variables, properties, default, parameterized constructors to Flight.cs
    Add default constructor to FlightManager.cs
    Add populateFlights method to FlightManagers.cs which reads flights.csv, splits the values and populates each flight and adds to flights list
    Add GetFlights method to FlightManagers.cs which returns the flights list
    Edit Flights.razor. All flights in csv file displayed in dropdown list. Finds flight based on origin, destination and day entered and displays in dropdown menu.

---
+++ FlightReservations/Data/Flight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightReservations.Data
+{
+    public class Flight
+    {
+        private string flight_code;
+        private string airline;
+        private string origin_airport;
+        private string destination_airport;
+        private string day;
+        private string time;
+        private int num_seats;
+        private double cost;
+
+        public string Flight_Code { get { return flight_code; } set { flight_code = value; } }
+        public string Airline { get {  return airline; } set {  airline = value; } }
+        public string Origin_Airport { get { return origin_airport; } set { origin_airport = value; } }
+        public string Destination_Airport { get { return destination_airport; } set { destination_airport = value; } }
+        public string Day { get { return day; } set { day = value; } }
+        public string Time { get { return time; } set { time = value; } }
+        public int Num_Seats { get { return num_seats; } set { num_seats = value; } }
+        public double Cost { get { return cost; } set { cost = value; } }
+        //Default Constructor
+        public Flight()
+        {
+        }
+        //Parameterized Constructor
+        public Flight(string flight_code, string airline, string origin_airport, string destination_airport, string day, string time, int num_seats, double cost)
+        {
+            this.Flight_Code = flight_code;
+            this.Airline = airline;
+            this.Origin_Airport = origin_airport;
+            this.Destination_Airport = destination_airport;
+            this.Day = day;
+            this.Time = time;
+            this.Num_Seats = num_seats;
+            this.Cost = cost;
+        }
+    }
+}
---
+++ FlightReservations/Data/FlightManager.cs
@@ -0,0 +1,55 @@
+﻿using FlightReservations.Components.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightReservations.Data
+{
+    public class FlightManager
+    {
+        string FLIGHT_TXT = "C:\\Users\\Amrit\\source\\repos\\Assginment2_FlightReservation\\FlightReservations\\flights.csv";
+        public static List<Flight> Flights = new List<Flight>();
+
+        public FlightManager()
+        {
+            populateFlights();
+        }
+        public void populateFlights()
+        {
+            Flight flight;
+            foreach(string line in File.ReadLines(FLIGHT_TXT))
+            {
+                string[] parts = line.Split(",");
+                string flight_code = parts[0];
+                string airline = parts[1];
+                string origin_airport = parts[2];
+                string destination_airport = parts[3];
+                string day = parts[4];
+                string time = parts[5];
+                int num_seats = int.Parse(parts[6]);
+                double cost = double.Parse(parts[7]);
+
+                flight = new Flight(flight_code,airline,origin_airport,destination_airport,day,time,num_seats,cost);
+                Flights.Add(flight);
+            }
+        }
+        public static List<Flight> GetFlights()
+        {
+            return Flights;
+        }
+        public List<Flight> findFlights(string origin, string destination, string day)
+        {
+            List<Flight> matchingFlights = new List<Flight>();
+            foreach (Flight f in Flights)
+            {
+                if(f.Origin_Airport == origin && f.Destination_Airport == destination && f.Day == day)
+                {
+                    matchingFlights.Add(f);
+                }
+            }
+            return matchingFlights;
+        }
+    }
+}
